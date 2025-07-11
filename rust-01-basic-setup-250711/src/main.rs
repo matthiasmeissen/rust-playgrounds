3f56d7ce1f6b10a5cf7d1fb946d57e59218a28b4
matthiasmeissen <meissenmatthias@gmail.com>
2025-07-11 16:45:30 +0200

    Add Project 01

---
+++ rust-01-basic-setup-250711/src/main.rs
@@ -0,0 +1,8 @@
+// The line fn main() {} defines a function called main
+// This is the first one that gets executed when to program runs
+// The ! after a function name means it calls a macro instead of a normal function
+// Use rustc <filename> to compile the program
+
+fn main() {
+    println!("Hello Rust.");
+}
