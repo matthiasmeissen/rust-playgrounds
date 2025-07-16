e978a684138e7633c9d95c1d18c59faada574a50
matthiasmeissen <meissenmatthias@gmail.com>
2025-07-16 20:37:13 +0200

    Add Project 06

---
+++ rust-06-scalar-types-250716/main.rs
@@ -0,0 +1,8 @@
+
+fn main() {
+    let int: u16 = 400;
+    println!("This is an usigned 16 bit integer: {int}");
+
+    let float: f32 = -24.37829;
+    println!("This is an signed 32 bit float: {float}");
+}
