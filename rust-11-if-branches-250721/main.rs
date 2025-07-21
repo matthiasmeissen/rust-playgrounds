acd22ddc2647a30a037d1115a579377ee613e8b7
matthiasmeissen <meissenmatthias@gmail.com>
2025-07-21 19:55:52 +0200

    Add Project 11

---
+++ rust-11-if-branches-250721/main.rs
@@ -0,0 +1,14 @@
+
+fn main() {
+    let num = 4
+    let result = isEven(num);
+    println!("The number {x} is even is {result}");
+}
+
+fn isEven(x: i32) -> bool {
+    if (x % 1 = 0) {
+        true
+    } else {
+        false
+    }
+}
