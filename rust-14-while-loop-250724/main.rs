e9a558b7957a3794b8a8b6e4d9183ddb7172a75c
matthiasmeissen <meissenmatthias@gmail.com>
2025-07-24 22:21:14 +0200

    Add Project 14

---
+++ rust-14-while-loop-250724/main.rs
@@ -0,0 +1,14 @@
+
+fn main() {
+    // Declare mutable variable and initialize with 0
+    let mut x = 0;
+    // Create a while loop that runs its content as long as condition is true
+    while x < 5 {
+        // Increment x by 1
+        x += 1;
+        // Print x with println!() macro
+        println!("{x}")
+    }
+    // Print line with done
+    println!("Done");
+}
