e7785df2a3723721cb2c6da746404418ca62af98
matthiasmeissen <meissenmatthias@gmail.com>
2025-07-25 15:30:23 +0200

    Add Project 15

---
+++ rust-15-for-loop-250725/main.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+    // Declare a variable and initialize with and array of numbers
+    let nums = [2, 4, 8, 16, 32];
+    // Use for loop to iterate through the array
+    for num in nums {
+        // Print the item at index of array
+        println!({num});
+    }
+}
