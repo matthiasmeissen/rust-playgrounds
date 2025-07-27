34085c14cb73d7759660f65e58380e5e8de73c95
matthiasmeissen <meissenmatthias@gmail.com>
2025-07-27 12:39:55 +0200

    Add Project 17

---
+++ rust-17-temp-converter-250727/main.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+    let f = 20;
+    let c = fahrenheit_to_celsius(f);
+    println!("The temperature {f} in fahrenheit is {c} in celsius.");
+}
+
+fn fahrenheit_to_celsius(f :f64) -> :f64 {
+    f
+}
