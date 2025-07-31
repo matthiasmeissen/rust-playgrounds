f7809e0e4b65128ab4ddc7c15ddc59e2a91de81d
matthiasmeissen <meissenmatthias@gmail.com>
2025-07-31 20:26:14 +0200

    Add Project 20

---
+++ rust-20-ownership-move-250731/main.rs
@@ -0,0 +1,6 @@
+
+fn main() {
+    // Declare a mutable variable and initialize with pointer to a value of type String, stored on heap memory
+    let mut s1 = String::from("Hello");
+    println!("{s1}");
+}
