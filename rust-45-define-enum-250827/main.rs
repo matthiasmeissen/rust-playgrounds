a81b9d1de51516529468b0bbf60413865fad71a9
matthiasmeissen <meissenmatthias@gmail.com>
2025-08-27 21:31:31 +0200

    Add Project 45

---
+++ rust-45-define-enum-250827/main.rs
@@ -0,0 +1,23 @@
+
+// An Enum (Enumeration) is a collection of variants that belong together
+// We enumerate all possible variants
+
+// This enum create a custom data type called IpAddrKind
+// It has the variants V4 and V6
+// The V4 consists of four unsigned 8bit integers (0-255)
+// The V6 is one String
+#(derive(Debug))
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+fn main() {
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+
+    println!(
+        "The IP Address of home is: {:?} \nThe IP Address of loopback is: {:?}"
+        , home, loopback
+    );
+}
