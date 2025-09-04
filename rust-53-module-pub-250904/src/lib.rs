ad156aeb07e21689814bba1298846d7db585379c
matthiasmeissen <meissenmatthias@gmail.com>
2025-09-04 10:33:04 +0200

    Add Project 53

---
+++ rust-53-module-pub-250904/src/lib.rs
@@ -0,0 +1,6 @@
+
+pub mod front_of_house {
+    pub fn seat_guest() {
+        println!("Guest seated.");
+    };
+}
---
+++ rust-53-module-pub-250904/src/main.rs
@@ -0,0 +1,6 @@
+
+use crate::front_of_house::seat_guest;
+
+fn main() {
+    seat_guest();
+}
