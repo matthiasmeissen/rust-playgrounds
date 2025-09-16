42e9e63ad6611942ae5e2b398e901dce6f284f78
matthiasmeissen <meissenmatthias@gmail.com>
2025-09-16 20:30:52 +0200

    Add Project 65

---
+++ rust-65-employee-database-250916/src/main.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+    println!("Enter details");
+    // Request use to enter something, there are two options
+    // <Add><Name>to<Department>
+    // <Get Data>
+
+    println!("What do you want to do: \n1: Add Employee \n2: Get data");
+
+}
