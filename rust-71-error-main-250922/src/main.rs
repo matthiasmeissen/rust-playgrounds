f191e98911f1a0f211bc1f855d906b6e08f1e77e
matthiasmeissen <meissenmatthias@gmail.com>
2025-09-22 21:52:19 +0200

    Add Project 71

---
+++ rust-71-error-main-250922/src/main.rs
@@ -0,0 +1,9 @@
+
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut username = String::new();
+    std::fs::File::open("src/hello.txt")?.read_to_string(&mut username)?;
+    println!("The username is {username}");
+    Ok(())
+}
