43ecbb17929e70c95f4ba96e5a7879c02e5c4ce5
matthiasmeissen <meissenmatthias@gmail.com>
2025-10-05 21:37:11 +0200

    Add Project 81

---
+++ rust-81-lifetime-syntax-251005/src/main.rs
@@ -0,0 +1,12 @@
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "abc".to_string();
+
+    let result = longest(&string1, &string2);
+    println!("The longer string is: {}", result);
+}
+
+fn longest (a: &str, b: &str) -> &str {
+    if a.len() > b.len() { a } else { b }
+}
