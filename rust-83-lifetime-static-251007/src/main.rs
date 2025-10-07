c5b5a72a74befa59849657d765d6adf58cf98d0a
matthiasmeissen <meissenmatthias@gmail.com>
2025-10-07 14:15:43 +0200

    Add Project 83

---
+++ rust-83-lifetime-static-251007/src/main.rs
@@ -0,0 +1,12 @@
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "abc".to_string();
+
+    let result = longest(&string1, &string2);
+    println!("The longer string is: {}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() { x } else { y }
+}
