55e9b7253f4ecbf24340b99b6e67e2abec60e006
matthiasmeissen <meissenmatthias@gmail.com>
2025-10-07 14:35:38 +0200

    Finish Lifetime Static

--- rust-83-lifetime-static-251007/src/main.rs
+++ rust-83-lifetime-static-251007/src/main.rs
@@ -1,12 +1,27 @@
+use std::fmt::Display;
 
 fn main() {
     let string1 = String::from("abcd");
     let string2 = "abc".to_string();
 
-    let result = longest(&string1, &string2);
+    // The 'static lifetime annotation is special an guarantees that it will live for the entire duration of the program
+    let string3: &'static str = "This has a static lifetime";
+
+    let result = longest(&string1, &string2, "Details");
     println!("The longer string is: {}", result);
+
+    println!("Static str: {}", string3);
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() { x } else { y }
+// Define lifetime 'a and assign to input and output of function
+// It makes sure that all elements with that lifetime attatched live at max as long as the shortest of all
+// The generic parameter T is connected to the trait Display
+// This means it can accept any type that can be formatted
+fn longest<'a, T>(x: &'a str, y: &'a str, annotation: T) -> &'a str 
+where 
+    T: Display 
+{
+    let return_value = if x.len() > y.len() { x } else { y };
+    println!("Result with {} is: {}", annotation, return_value);
+    return_value
 }
