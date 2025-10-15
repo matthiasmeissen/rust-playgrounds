254b76a8f41b721f5004201a97e91246d86987a1
matthiasmeissen <meissenmatthias@gmail.com>
2025-10-15 12:34:50 +0200

    Add Project 88

---
+++ rust-88-commandline-test-251015/src/main.rs
@@ -0,0 +1,21 @@
+use minigrep::*;
+
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::create(&args).unwrap_or_else(|err| {
+        eprintln!("Error creating the config: {err}");
+        process::exit(1);
+    });
+
+    let run_result = run(config);
+    match run_result {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("Application error {e}");
+            process::exit(1);
+        } 
+    }
+}
