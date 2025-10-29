2952b02cde6d1623f8d7a24f04f3f7f8a4bcda27
matthiasmeissen <meissenmatthias@gmail.com>
2025-10-29 11:24:30 +0100

    Add Project 97

---
+++ rust-97-test-basics-251029/src/main.rs
@@ -0,0 +1,19 @@
+
+fn main() {
+    
+}
+
+pub fn add_two(x: i32) -> i32 {
+    x + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn adds_two() {
+        let result = add_two(3);
+        assert_eq!(result, 5);
+    }
+}
